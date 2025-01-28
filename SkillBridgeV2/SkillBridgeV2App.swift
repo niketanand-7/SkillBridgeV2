ffe4f12f96cf9c527e3f3a1d38eb2fe20be5b341
niketanand-7 <niketanand7@gmail.com>
2025-01-28 16:03:56 -0500

    Initial Commit

---
+++ SkillBridgeV2/SkillBridgeV2App.swift
@@ -0,0 +1,17 @@
+//
+//  SkillBridgeV2App.swift
+//  SkillBridgeV2
+//
+//  Created by Niket Anand on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct SkillBridgeV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SkillBridgeV2Tests/SkillBridgeV2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  SkillBridgeV2Tests.swift
+//  SkillBridgeV2Tests
+//
+//  Created by Niket Anand on 1/28/25.
+//
+
+import Testing
+@testable import SkillBridgeV2
+
+struct SkillBridgeV2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
