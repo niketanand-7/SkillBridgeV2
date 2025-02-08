ea7120320c5d37f8b62b9bfef933f925e90bfef4
niketanand-7 <niketanand7@gmail.com>
2025-02-08 01:50:59 -0500

    remove keys

--- SkillBridgeV2/SupabaseManager.swift
+++ SkillBridgeV2/SupabaseManager.swift
@@ -8,8 +8,4 @@
 import Supabase
 import Foundation
 
-let supabase = SupabaseClient(
-    supabaseURL: URL(string: "https://kweidqrgcviukknhspte.supabase.co")!,
-    supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt3ZWlkcXJnY3ZpdWtrbmhzcHRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg0NDQxMjIsImV4cCI6MjA1NDAyMDEyMn0.StoDEwNEHdrsXNIqzFP-GlCvzUL4e4iY5BCJDJ7_geY"
-)
 
